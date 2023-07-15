3f8071decd1f02639884d9756c8147813545d7c1
Abhiraj Rik <133798548+abrik1@users.noreply.github.com>
2023-07-15 13:06:51 +0530

    emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,20 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(xresources))
+ '(custom-safe-themes
+   '("e5494adf200eeff1505839672150dde6053e086869189c381b1ce9b792dda3a8" default))
+ '(package-selected-packages '(rainbow-delimiters eat xresources-theme ewal)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
