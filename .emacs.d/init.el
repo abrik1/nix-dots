65a08c60475015da7589980e9af79c20e05b5627
Abhiraj Rik <133798548+abrik1@users.noreply.github.com>
2023-07-16 12:34:21 +0530

    add some stuff
    - rainbow-delimeters
    - show line numbers
    - dashboard
    - marginalia

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,7 +8,8 @@
  '(custom-enabled-themes '(xresources))
  '(custom-safe-themes
    '("e5494adf200eeff1505839672150dde6053e086869189c381b1ce9b792dda3a8" default))
- '(package-selected-packages '(rainbow-delimiters eat xresources-theme ewal)))
+ '(package-selected-packages
+   '(nix-mode marginalia dashboard poke-line rainbow-delimiters eat xresources-theme ewal)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -18,3 +19,33 @@
 
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
+
+(require 'dashboard)
+(dashboard-setup-startup-hook)
+;; Or if you use use-package
+(use-package dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook))
+
+(setq dashboard-startup-banner "~/Pictures/EmacsLogo.jpeg")
+(setq dashboard-center-content t)
+
+;; Enable rich annotations using the Marginalia package
+(use-package marginalia
+  ;; Bind `marginalia-cycle' locally in the minibuffer.  To make the binding
+  ;; available in the *Completions* buffer, add it to the
+  ;; `completion-list-mode-map'.
+  :bind (:map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+
+  ;; The :init section is always executed.
+  :init
+
+  ;; Marginalia must be activated in the :init section of use-package such that
+  ;; the mode gets enabled right away. Note that this forces loading the
+  ;; package.
+  (marginalia-mode))
+
+(global-display-line-numbers-mode)
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
